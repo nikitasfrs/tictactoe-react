ad61a86ebc0fd4fd1c0d3a2ea515112418e1e09c
Nikitas <nikitas.frantzeskakis@gmail.com>
2015-11-29 17:23:55 +0000

    Updated build target with new app file

---
+++ main.js
@@ -0,0 +1,44 @@
+/**
+ *
+ * Reactive Tic-Tac-Toe UI
+ * Author: Nikitas Frantzeskakis
+ *
+ */
+
+var React = require('react'),
+    ReactDOM = require('react-dom'),
+    Game = require('./models/game'),
+    Player = require('./models/player'),
+  
+    GameControls = require('./components/GameControls')({
+        React: React
+    }),
+
+    GameCell = require('./components/GameCell')({
+        React: React
+    }),
+    GameRow = require('./components/GameRow')({
+        React: React,
+        GameCell: GameCell
+    }),
+    GameBoard = require('./components/GameBoard')({
+        React: React,
+        GameRow: GameRow
+    }),
+    TicTacToe = require('./components/TicTacToe')({
+        React: React,
+        GameBoard: GameBoard,
+        GameControls: GameControls
+    }),
+
+    players = [
+        new Player('Player 1', 'X'),
+        new Player('Player 2', 'O')
+    ],
+    game = new Game(players);
+
+ReactDOM.render(
+    <TicTacToe initGame={game}/>,
+    document.getElementById('app')
+);
+
